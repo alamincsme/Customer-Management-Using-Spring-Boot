d3384ab0980a91af936425a01858d68a26da5535
alamincsme <alamincsme@gmail.com>
2024-02-22 09:49:23 +0600

    Customer Management System Using Spring Boot

---
+++ src/main/java/io/alamincsme/CustomerManagementUsingSpringBootApplication.java
@@ -0,0 +1,14 @@
+package io.alamincsme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerManagementUsingSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerManagementUsingSpringBootApplication.class, args);
+
+    }
+
+}
